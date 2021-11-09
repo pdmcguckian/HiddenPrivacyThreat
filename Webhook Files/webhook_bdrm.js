da821ded07230f614bbf637d2f17b0adb3be27db
pdmcguckian <59895161+pdmcguckian@users.noreply.github.com>
2021-11-09 10:03:27 +0000

    FitBit API Trigger Running

---
+++ Webhook Files/webhook_bdrm.js
@@ -0,0 +1,27 @@
+exports = function(payload,response) {
+  
+  const mongodb = context.services.get("mongodb-atlas");
+  const sensordata = mongodb.db("SIoT").collection("BLEData");
+  
+  var body = {};
+  if (payload.body) {
+    
+    try{
+      
+    const document = EJSON.parse(payload.body.text());
+    
+    sensordata.updateOne(
+                {"timeStampID":document.timeStampID}, //Filter
+                {$set: {"rssiBDRM": document.rssiBDRM}},
+                {upsert:true} //Options
+              
+                 ).then(result => {
+    response.setStatusCode(201);
+  });
+    }
+    catch(err) {
+      console.log(err);
+      response.setStatusCode(500);
+    }}
+  
+}
