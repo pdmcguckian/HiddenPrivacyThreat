b2aa578442676408262800fa4c380ffc962893af
pdmcguckian <59895161+pdmcguckian@users.noreply.github.com>
2021-11-14 22:39:20 +0000

    WebApp Graphing in 24H

--- Webhook Files/webhook_collectBLE.js
+++ Webhook Files/webhook_collectBLE.js
@@ -1,5 +1,12 @@
 exports = function(payload) {
-	   const mongodb = context.services.get("mongodb-atlas");
-	   const mycollection = mongodb.db("SIoT").collection("BLEData");
-	   return mycollection.find({}).limit(1440).toArray();
+    
+    const document = EJSON.parse(payload.body.text());
+    
+    var start_time = Number(document.date) - 43200;
+    
+    console.log(start_time);
+
+    const mongodb = context.services.get("mongodb-atlas");
+	  const mycollection = mongodb.db("SIoT").collection("BLEData");
+    return mycollection.find({ timeStampID: { $gt: start_time } }).limit(1440).toArray();
 	};
