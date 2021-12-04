32d8c8da0d98f265b88e108f686105983f41344c
pdmcguckian <59895161+pdmcguckian@users.noreply.github.com>
2021-12-04 17:04:56 +0000

    Working 24H Graphs

--- Webhook Files/webhook_collectBLE.js
+++ Webhook Files/webhook_collectBLE.js
@@ -3,10 +3,11 @@ exports = function(payload) {
     const document = EJSON.parse(payload.body.text());
     
     var start_time = Number(document.date) - 43200;
+    var end_time = Number(document.date) + 43200;
     
     console.log(start_time);
 
     const mongodb = context.services.get("mongodb-atlas");
-	  const mycollection = mongodb.db("SIoT").collection("BLEData");
-    return mycollection.find({ timeStampID: { $gt: start_time } }).limit(1440).toArray();
+	const mycollection = mongodb.db("SIoT").collection("BLEData");
+    return mycollection.find({ timeStampID: { $gt: start_time, $lt: end_time} }).sort({timeStampID:1}).toArray();
 	};
