f9274a3f3b0da175365c680330a71b4c257baf80
pdmcguckian <59895161+pdmcguckian@users.noreply.github.com>
2021-12-12 12:15:00 +0000

    Machine Learning System Developed

--- Modeling/ml.py
+++ Modeling/ml.py
@@ -1,16 +1,19 @@
 import pymongo
 import csv
+import pandas as pd
+from sklearn import svm, tree
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import cross_val_score
 
-client = pymongo.MongoClient("mongodb+srv://pdmcguckian:JeFrY#2618@siot.gashn.mongodb.net/SIoT?retryWrites=true&w=majority")
-db = client["SIoT"]
-col = db["BLEData"]
 
-start = 1636632000
-end = 1637928000
-with open('/Users/pdmcguckian/Desktop/newdata.csv', 'r') as file:
+
+
+dimension_reduced_ble = {}
+with open('/Users/pdmcguckian/Documents/IoT/Project/SIoT/Modeling/BLEData.csv', 'r') as file:
     count = 0
-    date = 11
-    values = {}
+    date = 12
     bdrm = 0
     lvngrm = 0
     ktchn = 0
@@ -19,8 +22,7 @@ with open('/Users/pdmcguckian/Desktop/newdata.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
         if count == 1440:
-            if real:
-                values[date] = [timein, ktchn, lvngrm, bdrm]
+            dimension_reduced_ble[date] = [timein, lvngrm, bdrm, ktchn]
             count = 0
             bdrm = 0
             lvngrm = 0
@@ -28,35 +30,73 @@ with open('/Users/pdmcguckian/Desktop/newdata.csv', 'r') as file:
             date += 1
             timein = 0
             real = False
-        
+
         count +=1
 
         try:
             lvngrmRSSI = int(row[1])
-            bdrmRSSI = int(row[2])
-            ktchnRSSI = int(row[3])
-
-            if (bdrmRSSI == -100 and lvngrmRSSI == -100 and ktchnRSSI == -100):
-                pass
-
-            elif (bdrmRSSI >= ktchnRSSI and bdrmRSSI >= lvngrmRSSI):
-                bdrm+=1
-
-                if not real:
-                    timein = count
-                real = True
-
-            elif (lvngrmRSSI >= bdrmRSSI and lvngrmRSSI >= ktchnRSSI):
-                lvngrm += 1
-                if not real:
-                    timein = count
-                real = True
-
-            elif (ktchnRSSI >= bdrmRSSI and ktchnRSSI >= lvngrmRSSI):
-                ktchn+=1
-                if not real:
-                    timein = count
-                real = True
-
         except:
+            lvngrmRSSI = -100
+        
+        try:
+            bdrmRSSI = int(row[2])
+        except:
+            bdrmRSSI = -100
+        
+        try:
+            ktchnRSSI = int(row[3])
+        except:
+            ktchnRSSI = -100
+
+        if (bdrmRSSI == -100 and lvngrmRSSI == -100 and ktchnRSSI == -100):
             pass
+
+        elif (bdrmRSSI >= ktchnRSSI and bdrmRSSI >= lvngrmRSSI):
+            bdrm+=1
+
+            if not real:
+                timein = count
+            real = True
+
+        elif (lvngrmRSSI >= bdrmRSSI and lvngrmRSSI >= ktchnRSSI):
+            lvngrm += 1
+            if not real:
+                timein = count
+            real = True
+
+        elif (ktchnRSSI >= bdrmRSSI and ktchnRSSI >= lvngrmRSSI):
+            ktchn+=1
+            if not real:
+                timein = count
+            real = True
+        pass
+
+dataset = []
+with open('/Users/pdmcguckian/Documents/IoT/Project/SIoT/Modeling/PredictionData.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        date = int(row[0])
+        alcohol = int(row[1])
+        sleep = int(row[2])
+        instance = [alcohol, sleep, dimension_reduced_ble[date][0], dimension_reduced_ble[date][1], dimension_reduced_ble[date][2], dimension_reduced_ble[date][3]]
+        dataset.append(instance)
+
+
+df = pd.DataFrame(dataset, columns=['Alcohol', 'Sleep', 'TimeIn', 'LivingRoom', 'Bedroom', 'Kitchen'])
+df = df.sample(frac=1).reset_index(drop=True)
+y = df['Alcohol']
+X = df.drop(columns=["Alcohol", "Sleep"])
+
+print(X)
+print(y)
+
+clf = svm.SVC(kernel='linear', C=10)
+#clf = RandomForestClassifier(max_depth=2, min_samples_split=2, min_impurity_decrease=0)
+#clf = tree.DecisionTreeClassifier(max_depth = 2, min_impurity_decrease=0.0)
+#poly = PolynomialFeatures(degree = 2, interaction_only=False, include_bias=False)
+#X = poly.fit_transform(X)
+#clf = LogisticRegression(max_iter=1300, C=10)
+scores = cross_val_score(clf, X, y, cv=3)
+
+print(scores)
+print("%0.2f accuracy with a standard deviation of %0.2f" % (scores.mean(), scores.std()))
